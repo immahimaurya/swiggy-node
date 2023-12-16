173a429bcd9fd0a3a32791fdc70d33dd8d48146f
immahimaurya <immahimaurya7274@gmail.com>
2023-12-16 08:49:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = require("express").Router();
+const PORT = 8000;
+
+const app = express();
+
+app.listen(PORT, async () => {
+  console.log(`server up on port ${PORT}`);
+});
+
+router.get("/", (req, res) => {
+    res.send("Let's build a CRUD API!");
+});
